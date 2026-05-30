6dcf5bb9266f5fde83fc872643ace345e67e897d
Sruthi <sruthipedakolimi@gmail.com>
2026-05-30 13:50:46 -0500

    multi chat using the conversations API and responses API to persist conversation state

--- main.py
+++ main.py
@@ -8,21 +8,18 @@ api_key = os.getenv("OPEN_API_KEY")
 base_url = os.getenv("OPENAI_BASE_URL")
 client = OpenAI(api_key=api_key, base_url=base_url)
 messages = [{"role": "developer", "content": "You are a helpful assistant"}]
+
+conversation = client.conversations.create()
 while True:
-    user_input = input("Enter the prompt: ")
-    if user_input.lower() == "exit":
-        print("Exiting....Goodbye!!!!")
-        break
+    user_prompt = input("Enter your prompt: ")
+    if user_prompt.lower() == "exit":
+        print("Exiting....Goodbye!!!")
+        break 
 
-    user_prompt = {"role": "user", "content": user_input}
-    messages.append(user_prompt)
-    response = client.chat.completions.create(model="gpt-4o-mini", messages=messages)
-    new_message = {"role": response.choices[0].message.role, "content": response.choices[0].message.content}
-    messages.append(new_message)
-    print(new_message['content'])
-    print("")
-    
- 
-    
+    response = client.responses.create(
+        model="gpt-4.1",
+        input=[{"role": "user", "content": user_prompt}],
+        conversation=conversation.id
+        )
 
-
+    print(response.output[0].content[0].text)
