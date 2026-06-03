078b2d7e29a28c5ca30028af8c632cd5cd8190d2
Sruthi <sruthipedakolimi@gmail.com>
2026-06-03 16:08:16 -0500

    1. Added system prompt
    2. Clean code

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import openai
 from dotenv import load_dotenv
 from openai import OpenAI
 
-def error_handling(e):
+# get the error message from the error object
+def get_error_message(e):
     message = None
     if isinstance(e.body, dict):
         message = e.body.get("message", e)
@@ -12,44 +13,64 @@ def error_handling(e):
     return message
 
 # loading the .env file
+# override the global keys if needed
 load_dotenv(override=True)
 api_key = os.getenv("OPEN_API_KEY")
 base_url = os.getenv("OPENAI_BASE_URL")
+system_prompt = os.getenv("SYSTEM_PROMPT")
+
+# creating open AI object
 client = OpenAI(api_key=api_key, base_url=base_url)
 try:
-    conversation = client.conversations.create()
+    # creating the conversation with the system prompt
+    conversation = client.conversations.create(
+         items=[
+             {"type": "message", "role": "system", "content": system_prompt}
+            ]
+    )
+    # handling the conversation loop until user exit
     while True:
         user_prompt = input("Enter your prompt: ")
+
+        # handling exit condition
         if user_prompt.lower() == "exit":
             print("Exiting....Goodbye!!!")
             break 
+
+        # handling user empty input condition
         if user_prompt.strip() == "":
             print("Please enter the input")
             continue
+
+        # call response API with conversation id
         response = client.responses.create(
             model="gpt-4.1",
             input=[{"role": "user", "content": user_prompt}],
             conversation=conversation.id,
             stream=True
             )
+        
+        # print stream message
         for event in response:
             if event.type == "response.output_text.delta":
                 print(event.delta, end="", flush=True)
         print()
+
+# handling exceptions
 except openai.AuthenticationError as e:
-    message = error_handling(e)
+    message = get_error_message(e)
     print(f"Authentication failed: {message}")
 
 except openai.RateLimitError as e:
-     message = error_handling(e)
+     message = get_error_message(e)
      print(f"OpenAI API request exceeded rate limit: {message}")
 
 except openai.APITimeoutError as e:
-    message = error_handling(e)
+    message = get_error_message(e)
     print(f"Timeout error please try after sometime: {message}")
 
 except openai.APIConnectionError as e:
-    message = error_handling(e)
+    message = get_error_message(e)
     print(f"Network error please check and try again: {message}")
 except Exception as e:
     print(f"An error occured {e}")
